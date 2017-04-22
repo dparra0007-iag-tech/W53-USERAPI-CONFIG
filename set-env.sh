23831669cbff337216d0cc7c29bc418a3c92ba5e
David Parra Perez <david.parra@outlook.com>
2017-04-23 01:31:05 +0200

    add to bashrc

--- set-env.sh
+++ set-env.sh
@@ -3,3 +3,5 @@ echo export NAME="david" >> /etc/bash.bashrc
 echo export APELLIDO="parra" >> /etc/bash.bashrc
 echo export NAME="david" >> /etc/environment
 echo export APELLIDO="parra" >> /etc/environment
+echo export NAME="david" >> ~/.bashrc
+echo export APELLIDO="parra" >> ~/.bashrc
