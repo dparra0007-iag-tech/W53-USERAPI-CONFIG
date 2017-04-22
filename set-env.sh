59b702d36b63d5ecfe53bf0aa368e2fce4b6e287
David Parra Perez <david.parra@outlook.com>
2017-04-22 16:40:07 +0200

    try export to app env

--- set-env.sh
+++ set-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo export NAME="david" >> /etc/bash.bashrc
 echo export APELLIDO="parra" >> /etc/bash.bashrc
-echo export NAME="david" > /etc/profile.d/docker_init.sh
-echo export APELLIDO="parra" > /etc/profile.d/docker_init.sh
+export NAME="david"
+export APELLIDO="parra"
