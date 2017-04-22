7ee8aa93bb3e9ede012cb23ca717ceee470581ae
David Parra Perez <david.parra@outlook.com>
2017-04-22 16:24:58 +0200

    set env for docker user

--- set-env.sh
+++ set-env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo export NAME="david" >> /etc/bash.bashrc
 echo export APELLIDO="parra" >> /etc/bash.bashrc
+echo export NAME="david" > /etc/profile.d/docker_init.sh
+echo export APELLIDO="parra" > /etc/profile.d/docker_init.sh
