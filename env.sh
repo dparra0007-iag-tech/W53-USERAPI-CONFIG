a33cb17581ae71f28e16c24fad0881a31cad7361
David Parra Perez <david.parra@outlook.com>
2017-04-24 11:47:21 +0200

    fix not a valid identifier

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export USERAPI-ERRMESSAGE="Sorry, but some error have happened"
-export USERAPI-ADDMESSAGE="Added"
-export USERAPI-DELMESSAGE="Deleted"
-export USERAPI-MONGODBENDPOINT="userapi-db:27017/nodetest2"
+export USERAPIERRMESSAGE="Sorry, but some error have happened"
+export USERAPIADDMESSAGE="Added"
+export USERAPIDELMESSAGE="Deleted"
+export USERAPIMONGODBENDPOINT="userapi-db:27017/nodetest2"
