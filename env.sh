afbfc11bbbc1ba2ecc62115b4756c265d8914b46
David Parra Perez <david.parra@outlook.com>
2017-04-24 15:10:07 +0200

    change messages

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export USERAPI_ERRMESSAGE="Sorry, but some error have happened"
-export USERAPI_ADDMESSAGE="Added"
-export USERAPI_DELMESSAGE="Deleted"
+export USERAPI_ADDMESSAGE="User added"
+export USERAPI_DELMESSAGE="user deleted"
