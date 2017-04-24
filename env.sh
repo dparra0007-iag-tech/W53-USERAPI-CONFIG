c27fdd3f4319c72ed8e5e585388cd126b3197826
David Parra Perez <david.parra@outlook.com>
2017-04-24 11:41:36 +0200

    naming for scale

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export ERRMESSAGE="Sorry, but some error have happened"
-export ADDMESSAGE="Added"
-export DELMESSAGE="Deleted"
-export MONGODBENDPOINT="userapi-db:27017/nodetest2"
+export USERAPI-ERRMESSAGE="Sorry, but some error have happened"
+export USERAPI-ADDMESSAGE="Added"
+export USERAPI-DELMESSAGE="Deleted"
+export USERAPI-MONGODBENDPOINT="userapi-db:27017/nodetest2"
